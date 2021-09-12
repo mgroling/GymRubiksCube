8f43de1f79ec5b583a30482617dd18b4bef93252
marc131183 <marc.groeling@gmail.com>
2021-09-12 18:30:50 +0200

    moved general-use functions to seperate file

--- render.py
+++ render.py
@@ -1,88 +1,7 @@
-from abc import get_cache_token
-
-from numpy.lib.polynomial import poly
-from main import Cube
 import numpy as np
 import pygame
 import objects3D
-
-
-def getQuadrant(x: float, y: float):
-    if x >= 0 and y >= 0:
-        return 1
-    elif x > 0 and y < 0:
-        return 2
-    elif x <= 0 and y <= 0:
-        return 3
-    elif x < 0 and y < 0:
-        return 4
-
-
-def doesLineSegmentIntersectRayFromPoint(
-    point: np.array,
-    ray_direction: np.array,
-    lineSegment_start: np.array,
-    lineSegment_end: np.array,
-):
-    # compute intersection of them and check if it's between line start and end
-    # check if ray and line are parallel (if A is invertible)
-    A = np.array([(lineSegment_end - lineSegment_start), -ray_direction]).T
-    if np.linalg.det(A) == 0:
-        return False
-    b = point - lineSegment_start
-    sol = np.linalg.solve(A, b)
-    if sol[0] >= 0 and sol[0] <= 1 and sol[1] >= 0:
-        return True
-    else:
-        return False
-
-
-def isVertexInsidePolygon2D(vertex: np.array, lines: list):
-    # if ray intersects an even number of times, then vertex is outside of polygon
-    intersections = 0
-    for line in lines:
-        print("current line", line)
-        if doesLineSegmentIntersectRayFromPoint(
-            vertex, np.array([1, 1]), line[0], line[1]
-        ):
-            intersections += 1
-            print("intersects!")
-
-    # in theory, we'd have to subtract 1 if the ray goes through an exact point per point, but it's very unlikely so we can ignore it for now
-
-    print("num intersections", intersections)
-    if intersections % 2 == 0:
-        return False
-    else:
-        return True
-
-
-def isAnyFaceOverVertex(faces: list, vertex: np.array):
-    for face in faces:
-        # create the lines between visible vertices of a given face (such that they are in order as given by the face)
-        visible_vertices_ordered = face.getPolygonOfSelf()
-        visible_lines = [
-            (visible_vertices_ordered[i], visible_vertices_ordered[i + 1])
-            for i in range(len(visible_vertices_ordered) - 1)
-        ] + [(visible_vertices_ordered[-1], visible_vertices_ordered[0])]
-        if isVertexInsidePolygon2D(vertex, visible_lines):
-            return True
-
-    return False
-
-
-class Plane:
-    def __init__(self, origin: np.array, vector1: np.array, vector2: np.array) -> None:
-        self.origin = origin
-        self.vector1 = vector1
-        self.vector2 = vector2
-
-    def getBarycentricCoordinates(self, point: np.array, direction: np.array):
-        a = np.array([self.vector1, self.vector2, -direction]).T
-        b = point - self.origin
-        sol = np.linalg.solve(a, b)
-
-        return np.array([sol[0], sol[1]])
+from functions import *
 
 
 class ProjectionRenderer:
@@ -144,7 +63,6 @@ class ProjectionRenderer:
             index_of_object = canvas_coords.index(object_with_min_vertex)
             min_vertex = object_with_min_vertex[0]
 
-            print("current vertex", min_vertex[0])
             if not isAnyFaceOverVertex(visible_faces, min_vertex[0]):
                 for face in objects[index_of_object].faces:
                     # set the vertex to visible in each face that contains it
@@ -209,5 +127,5 @@ if __name__ == "__main__":
                 over = True
 
         if first:
-            p.render(np.array([-800, -800, 400]), np.array([0, 0, 0]), [cube])
+            p.render(np.array([-800, -800, 0]), np.array([0, 0, 0]), [cube])
             first = False
