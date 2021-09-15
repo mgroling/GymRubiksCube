791650ab349118b6c940ca90ccd534988e88db26
marc131183 <marc.groeling@gmail.com>
2021-09-15 10:33:27 +0200

    created main file for better structure

--- src/render.py
+++ src/render.py
@@ -1,14 +1,10 @@
 import numpy as np
 import pygame
-import time
-import timeit
-import objects3D
 from functions import *
 
 
 class ProjectionRenderer:
     def __init__(self, canvas_distance: float, width: int, height: int) -> None:
-        super().__init__()
         self.switch = True
         self.last_quadrant = None
         self.canvas_distance = canvas_distance
@@ -105,16 +101,12 @@ class ProjectionRenderer:
         return visible_faces
 
     def render(self, pov: np.array, look_point: np.array, objects: list):
-        cur_time = time.time()
         canvas_coordinates, num_vertices_total = self.getCanvasCoordinates(
             pov, look_point, objects
         )
-        time_passed = time.time() - cur_time
-        cur_time = time.time()
         visible_faces = self.computeVisibleFaces(
             objects, canvas_coordinates, num_vertices_total
         )
-        time_passed = time.time() - cur_time
 
         visible_faces = [
             elem for elem in visible_faces if len(elem.getPolygonOfSelf()) == 4
@@ -136,75 +128,18 @@ class ProjectionRenderer:
         pygame.display.update()
 
 
-if __name__ == "__main__":
-    WIDTH, HEIGHT = 800, 800
-    p = ProjectionRenderer(100, WIDTH, HEIGHT)
-    colours = [
-        (255, 213, 0),
-        (0, 155, 72),
-        (200, 100, 0),
-        (185, 0, 0),
-        (0, 69, 173),
-        (255, 255, 255),
-    ]
-    objects_to_render = []
-    for x in [100, 0, -100]:
-        for y in [100, 0, -100]:
-            for z in [100, 0, -100]:
-                objects_to_render.append(
-                    objects3D.Cube(np.array([x, y, z]), 90, colours)
-                )
-    # cube1 = objects3D.Cube(np.array([100, 100, 0]), 80, colours)
-    # cube2 = objects3D.Cube(np.array([100, -100, 0]), 80, colours)
-    # objects_to_render = [cube1, cube2]
-    pygame.display.set_caption("Rubicks Cube")
-    sphere = Sphere(800)
-    look_point = np.array([0, 0, 0])
+class RaycastRenderer:
+    def __init__(self, canvas_distance: float, width: int, height: int) -> None:
+        self.switch = True
+        self.last_quadrant = None
+        self.canvas_distance = canvas_distance
+        pygame.init()
+        pygame.font.init()
+        self.width, self.height = width, height
+        self.dis = pygame.display.set_mode((width, height))
 
-    over = False
-    delta_theta, delta_phi = 0, 0
-    time_passed = None
-    myFont = pygame.font.SysFont("Comic Sans MS", 30)
-    while not over:
-        cur_time = time.time()
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                over = True
-            # key pressed
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_UP:
-                    delta_theta = -1
-                elif event.key == pygame.K_DOWN:
-                    delta_theta = 1
-                elif event.key == pygame.K_RIGHT:
-                    delta_phi = 1
-                elif event.key == pygame.K_LEFT:
-                    delta_phi = -1
-            # key released
-            elif event.type == pygame.KEYUP:
-                if (event.key == pygame.K_UP and delta_theta == -1) or (
-                    event.key == pygame.K_DOWN and delta_theta == 1
-                ):
-                    delta_theta = 0
-                elif (event.key == pygame.K_RIGHT and delta_phi == 1) or (
-                    event.key == pygame.K_LEFT and delta_phi == -1
-                ):
-                    delta_phi = 0
-            elif event.type == pygame.MOUSEWHEEL:
-                sphere.radius -= event.y * 20
+    def getColourMap(self, pov: np.array, look_point: np.array, objects: list):
+        pass
 
-        pov = sphere.rotate(delta_theta, delta_phi)
-
-        p.render(pov, look_point, objects_to_render)
-        if time_passed != None:
-            text_surface = myFont.render(
-                "FPS {}".format(int(1 / time_passed)), False, (255, 255, 255)
-            )
-            p.dis.blit(text_surface, (10, 10))
-            pygame.display.update()
-
-        time_passed = time.time() - cur_time
-        wait = 0.0166666666 - time_passed
-        # 60 fps
-        if wait > 0:
-            time.sleep(wait)
+    def render(self):
+        pass
