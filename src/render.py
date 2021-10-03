fixed rotation of cube sides bug

src/render.py
@@ -385,7 +385,7 @@ class Scene:
     def rotateObjects(
         self, objects_ids: List[int], axis: int, rotation_angle: float
     ) -> None:
-        """rotates objects given by their id around the axis (0, 1 or 2) with the given rotation angle"""
+        """rotates objects given by their id around the axis (0, 1 or 2) with the given rotation angle (around the coordinate center)"""
         rot = RotationMatrix3D()
         for obj_id in objects_ids:
             triangles = self.objects[obj_id]
@@ -399,6 +399,33 @@ class Scene:
                 self.triangle_vec2s[elem] = rot(
                     self.triangle_vec2s[elem], axis, rotation_angle
                 )
+
+    def _getObjectsCenters(self):
+        """for debugging"""
+        points = [[] for i in range(len(self.objects))]
+        for key, value in self.objects.items():
+            for ids in value:
+                points[key].append(self.triangle_origins[ids])
+                points[key].append(
+                    self.triangle_origins[ids] + self.triangle_vec1s[ids]
+                )
+                points[key].append(
+                    self.triangle_origins[ids] + self.triangle_vec2s[ids]
+                )
+            points[key] = np.mean(points[key], axis=0)
+
+        out = np.empty((3, 3, 3))
+        arrZY = [100, 0, -100]
+        arrZX = [-100, 0, 100]
+        for i, elem in enumerate(points):
+            index1, index2, index3 = (
+                arrZY.index(np.round(elem[2])),
+                arrZY.index(np.round(elem[1])),
+                arrZX.index(np.round(elem[0])),
+            )
+            out[index1, index2, index3] = i
+
+        return out
 
 
 if __name__ == "__main__":

src/rubicksCube.py
@@ -34,12 +34,13 @@ class RubicksCubeEnv(gym.Env):
         # 6 7 8
         self.structure = np.arange(27).reshape(3, 3, 3)
 
-    def step(self, action) -> np.ndarray:
+    def step(self, action: int) -> np.ndarray:
         # NOT AS GOOD; BETTER USE THIS: VECTOR REPRESENTATION (page 4)
         # https://dl.acm.org/doi/pdf/10.1145/800058.801107?casa_token=Qv5syAkQaZAAAAAA:JmbMhpHxjpeC8Oijx1_au6y-nNxiTfh9lfS6B06kUZQowLNixqXEqTxgNwOcKeTwP-oswhlvwH8
         if self.__setup_render:
             axis = action // 4
             index = action % 4 // 2 * 2
+            # rotate either counter-clockwise or clockwise (if action % 2 == 0 -> counter-clockwise, else clockwise)
             flip_axis = action % 2
             step_rot = 5 if action % 2 == 0 else -5
 
@@ -64,11 +65,11 @@ class RubicksCubeEnv(gym.Env):
                 self.structure[index] = np.flip(self.structure[index].T, axis=flip_axis)
             elif axis == 1:
                 self.structure[:, index] = np.flip(
-                    self.structure[:, index].T, axis=flip_axis
+                    self.structure[:, index].T, axis=1 - flip_axis
                 )
             else:
                 self.structure[:, :, index] = np.flip(
-                    self.structure[:, :, index].T, axis=flip_axis
+                    self.structure[:, :, index].T, axis=1 - flip_axis
                 )
 
     def _setup_render(self) -> None:
@@ -88,7 +89,7 @@ class RubicksCubeEnv(gym.Env):
         for z in [100, 0, -100]:
             for y in [100, 0, -100]:
                 for x in [-100, 0, 100]:
-                    col = [(0, 0, 0) for i in range(6)]
+                    col = [(0, 0, 0) for _ in range(6)]
                     if x < 0:
                         col[2] = colors[2]
                     elif x > 0:
@@ -181,12 +182,17 @@ class RubicksCubeEnv(gym.Env):
 if __name__ == "__main__":
     env = RubicksCubeEnv()
 
-    # env.step(4)
-    # env.step(5)
+    # env.render()
+    # env.step(0)
+    # env.render()
+    # env.step(8)
+    # env.render()
+    # env.step(0)
     # print(env.structure)
 
     i = 0
     while True:
         env.render()
-        env.step(i)
-        i = (i + 1) % 12
+        env.step(np.random.randint(12))
+        # env.step(i)
+        i = (i + 3) % 12