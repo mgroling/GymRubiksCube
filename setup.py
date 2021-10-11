099b97c7c756c96e4aa1522ee67903d0e22c9174
marc131183 <marc.groeling@gmail.com>
2021-10-11 16:04:08 +0200

    changed name of package

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name="rubiksCubeEnv",
+    name="GymRubiksCube",
     version="0.0.1",
     install_requires=["numpy", "numba", "pygame", "pillow", "scipy", "gym"],
 )
