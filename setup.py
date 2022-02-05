8d5ed4ed77151a2274ac4e15a71ec0f0c91b2209
marc131183 <marc.groeling@gmail.com>
2022-02-05 12:16:18 +0100

    specify versions

--- setup.py
+++ setup.py
@@ -3,5 +3,12 @@ from setuptools import setup
 setup(
     name="GymRubiksCube",
     version="0.0.1",
-    install_requires=["numpy", "numba", "pygame", "pillow", "scipy", "gym"],
+    install_requires=[
+        "numpy==1.20.3",
+        "numba==0.54.0",
+        "pygame==2.0.1",
+        "pillow==7.0.0",
+        "scipy==1.7.1",
+        "gym==0.20.0",
+    ],
 )
